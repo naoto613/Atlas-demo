fd4f607e146c4c4d4370b8bd8a6f9980a471c4ac
naoto613 <fujii140@gmail.com>
2022-05-26 13:20:53 +0900

    minor-chore 不要なファイルの削除とmain編集

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersResolverModule } from './infrastructure/ioc/resolvers/users.resolver.module';
+import { LoggingPlugin } from './infrastructure/plugins/logging.plugin';
+import { ComplexityPlugin } from './infrastructure/plugins/complexity.plugin';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(
+        process.cwd(),
+        'src/infrastructure/prisma/schema.gql',
+      ),
+    }),
+    UsersResolverModule,
+  ],
+  providers: [LoggingPlugin, ComplexityPlugin],
 })
 export class AppModule {}
