5c3ef151a3332993463a84f95fc5288b93b73b5d
naoto613 <fujii140@gmail.com>
2022-05-26 13:18:55 +0900

    patch-chore generateの実行

---
+++ src/infrastructure/prisma/@generated/prisma-nestjs-graphql/user/user-scalar-field.enum.ts
@@ -0,0 +1,16 @@
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum UserScalarFieldEnum {
+  id = 'id',
+  email = 'email',
+  name = 'name',
+  createdAt = 'createdAt',
+  updatedAt = 'updatedAt',
+  authority = 'authority',
+  hashedRefreshToken = 'hashedRefreshToken',
+}
+
+registerEnumType(UserScalarFieldEnum, {
+  name: 'UserScalarFieldEnum',
+  description: undefined,
+});
