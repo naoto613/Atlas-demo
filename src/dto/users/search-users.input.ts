17a8e8cddddc6112abc474b14ee47806537e9b5a
naoto613 <fujii140@gmail.com>
2022-05-31 19:56:14 +0900

    patch-chore nestの考慮を追加

--- src/dto/users/search-users.input.ts
+++ src/dto/users/search-users.input.ts
@@ -1,23 +1,13 @@
-import { ArgsType, Field, InputType, Int } from '@nestjs/graphql';
+import { ArgsType, Field, InputType } from '@nestjs/graphql';
 import { Authority } from '@prisma/client';
-import { Type } from 'class-transformer';
-import * as Validator from 'class-validator';
-import { Min, ValidateNested } from 'class-validator';
 
 @InputType()
 class SearchInput {
   @Field(() => String, { nullable: true })
-  @Validator.IsEmail()
   email?: string;
 
   @Field(() => String, { nullable: true })
   name?: string;
-
-  @Field(() => Int, { nullable: true })
-  @Validator.IsOptional()
-  // @Validator.MinLength(8)
-  @Min(1)
-  test?: number;
 
   @Field(() => String, { nullable: true })
   authority?: Authority;
@@ -26,8 +16,6 @@ class SearchInput {
 @ArgsType()
 export class SearchUsersInput {
   @Field(() => SearchInput, { nullable: true })
-  @ValidateNested()
-  @Type(() => SearchInput)
   searchInput?: SearchInput;
 
   @Field(() => Number, { nullable: true })
